8ee73a2885a5fb91deb331f7d0dec75ae68ec74a
Felscream <xblackfieldx@gmail.com>
2021-04-26 23:45:58 +0200

    bot status

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,10 @@ const commandHandler = new CommandHandler(Config.prefix);
 
 bot.on("ready", () => {
   console.log("bot is ready");
+  bot.user.setPresence({ status: "online" });
+  bot.user.setActivity(`${Config.prefix}help to view commands`, {
+    type: "WATCHING",
+  });
 });
 
 bot.on("message", (message) => {
