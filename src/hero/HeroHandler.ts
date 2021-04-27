6fa9ae455e9a125b0d6e2a01eb2c4ca9def66a13
Felscream <xblackfieldx@gmail.com>
2021-04-27 20:46:54 +0200

    Levensthein implementation to account for typos

--- src/hero/HeroHandler.ts
+++ src/hero/HeroHandler.ts
@@ -1,8 +1,10 @@
 import { Message, MessageEmbed } from "discord.js";
+import levensthein from "../utility/Levensthein";
 import createHeroEmbed from "../embeds/HeroEmbed";
 import { HeroListMenu } from "../menus/HeroListMenu";
 import { getHeroesList, getHeroById } from "./HeroService";
 import { HeroSummary } from "./model/heroSummary";
+import Config from "../../config.json";
 
 async function displayHeroes(message: Message, args: string[]) {
   const menu = new HeroListMenu(message.channel, message.author.id);
@@ -17,13 +19,7 @@ async function displayHero(args: string[]): Promise<MessageEmbed> {
     return;
   }
   const heroName = args.join(" ");
-  const requestedHero: HeroSummary = heroes.find(
-    (hero) =>
-      hero.name
-        .toLowerCase()
-        .localeCompare(heroName.toLowerCase(), "en", { sensitivity: "base" }) ==
-      0
-  );
+  let requestedHero: HeroSummary = findHeroWithName(heroName, heroes);
   if (requestedHero === undefined) {
     console.log("No result for search request '" + args.join(" ") + "'");
     return null;
@@ -35,6 +31,23 @@ async function displayHero(args: string[]): Promise<MessageEmbed> {
   }
 
   return null;
+}
+
+function findHeroWithName(input: string, heroes: HeroSummary[]): HeroSummary {
+  let score = Number.MAX_VALUE;
+  let hero: HeroSummary = undefined;
+  for (let i = 0; i < heroes.length; i++) {
+    let heroScore = levensthein(input, heroes[i].name, true);
+    if (heroScore == 0) {
+      return heroes[i];
+    }
+    if (heroScore <= Config.typoTolerance && heroScore < score) {
+      score = heroScore;
+      hero = heroes[i];
+    }
+  }
+
+  return hero;
 }
 
 export { displayHeroes, displayHero };
---
+++ src/utility/Levensthein.ts
@@ -0,0 +1,27 @@
+function levensthein(a: string, b: string, ignoreCase: boolean = false) {
+  const firstString = ignoreCase ? a.toLowerCase() : a;
+  const secondString = ignoreCase ? b.toLowerCase() : b;
+  var n = a.length,
+    m = b.length,
+    matrix: number[][] = [];
+  for (var i = -1; i < n; i++) {
+    matrix[i] = [];
+    matrix[i][-1] = i + 1;
+  }
+  for (var j = -1; j < m; j++) {
+    matrix[-1][j] = j + 1;
+  }
+  for (var i = 0; i < n; i++) {
+    for (var j = 0; j < m; j++) {
+      var cout = firstString.charAt(i) == secondString.charAt(j) ? 0 : 1;
+      matrix[i][j] = Math.min(
+        1 + matrix[i][j - 1],
+        1 + matrix[i - 1][j],
+        cout + matrix[i - 1][j - 1]
+      );
+    }
+  }
+  return matrix[n - 1][m - 1];
+}
+
+export default levensthein;
