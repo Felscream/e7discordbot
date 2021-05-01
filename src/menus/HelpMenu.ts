300cfa88d65dec129f7fadda7619a4250b6d97ec
Felscream <xblackfieldx@gmail.com>
2021-05-01 23:45:55 +0200

    Added report help menu

--- src/menus/HelpMenu.ts
+++ src/menus/HelpMenu.ts
@@ -23,11 +23,42 @@ class HelpMenu {
   }
 
   private generateHelpMenu(): any[] {
-    const content = new MessageEmbed().setTitle("Louis XVII Bot - Commands")
-      .setDescription(`**${config.prefix}heroes**\nLists available heroes\n\n
-      **${config.prefix}hero [hero name]**\nDisplays hero data`);
+    const heroesDescription = new Description(
+      "heroes",
+      "Lists available heroes"
+    );
+    const heroDescription = new Description(
+      "h|hero <hero name>",
+      "Displays hero data"
+    );
+    const reportDescription = new Description(
+      "report <speed>|<ally1>,<ally2>,<ally3>,<team cp>|<enemy1>,<enemy2>,<enemy3>, <enemy cp>|<match result>",
+      "Reports a guild war match results\n*speed* your fastest unit speed\n*Match result accepted values* : \n*VICTORY* -> w, v, win, victory, victoire\n*DRAW* -> e, draw, egalite\n*LOSS* -> l, loss, lose, defeat, defaite"
+    );
+
+    const commands = [heroesDescription, heroDescription, reportDescription];
+    const content = new MessageEmbed()
+      .setTitle("Louis XVII Bot - Commands")
+      .setDescription(this.buildHelpContent(commands));
 
     return [{ name: "help", content: content, reactions: {} }];
+  }
+
+  private buildHelpContent(descriptions: Description[]) {
+    let helpContent = "";
+    descriptions.forEach((description) => {
+      helpContent += `**${config.prefix}${description.command}**\n${description.description}\n\n`;
+    });
+    return helpContent;
+  }
+}
+
+class Description {
+  command: string;
+  description: string;
+  constructor(command: string, description: string) {
+    this.command = command;
+    this.description = description;
   }
 }
 
